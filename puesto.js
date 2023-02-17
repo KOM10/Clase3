1cf4a35aee6dbcb424b713c6a4d0c8854fb172d1
KOM10 <125660442+KOM10@users.noreply.github.com>
2023-02-17 16:27:56 -0500

    Update puesto.js

--- puesto.js
+++ puesto.js
@@ -7,23 +7,19 @@ const port = 3000;
 
 http.createServer((reques,Response)=>{
 
-    if(reques.url === '/'){
-        Response.write('inicio');
-        return Response.end();
-    }
 
     if(request.url === '/home'){
-        Response.write(fs.readFileSync('data/hola.html','utf-8'));
+        Response.write(fs.readFileSync('data/hola.html','utf-8'))
         return Response.end();
     }
 
-    // Response.write('gay kevin y andres');
+    // Response.write('kevin y andres');
     // Response.end()
 
-    // if(reques.url ==='/'){
-    //     Response.write('inicio de proyecto');
-    //     return Response.emd
-    // }
+    if(reques.url ==='/'){
+        Response.write('inicio de proyecto');
+        return Response.emd
+    }
 
 }).listen(port,host,()=>{
     console.log(`server is running on http://${host}:${port}/home`);
